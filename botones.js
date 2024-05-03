fe4ea1849cde675e4b04c10e7b0c2fd6fcc16dcc
CarlosAngul0 <168840376+CarlosAngul0@users.noreply.github.com>
2024-05-03 09:17:02 -0500

    comit

---
+++ botones.js
@@ -0,0 +1,12 @@
+document.getElementById("button1").addEventListener("click", function () {
+    window.location.href = "./op1/";
+  });
+document.getElementById("button2").addEventListener("click", function () {
+    window.location.href = "./op2/";
+  });
+document.getElementById("button3").addEventListener("click", function () {
+    window.location.href = "./op3/";
+  });
+document.getElementById("button4").addEventListener("click", function () {
+    window.location.href = "./op4/";
+  });
